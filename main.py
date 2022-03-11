cc3b508cc89a3c526427680b6c57437b89f42bc9
muhammad.ayyub <muhammad.ayyub@intern.bukalapak.com>
2022-03-12 01:01:25 +0700

    test batch on tokenizer word

--- main.py
+++ main.py
@@ -160,32 +160,35 @@ def _make_datasets_and_trainer(config, model, model_enum, tokenizer, task, task_
     
     # tokenize text and define datasets for word classification
     def preprocess_fn_word(examples):
-        tokenized_inputs = tokenizer(examples[task_data.sentence1_key], truncation=True, is_split_into_words=True)
-        label_all_tokens = True
-        labels = []
-        for i, label in enumerate(examples[TASK_LABELS[task]]):
-            word_ids = tokenized_inputs.word_ids(batch_index=i)
-            logger.info(tokenized_inputs)
-            logger.info(word_ids)
-            logger.info(label)
-            previous_word_idx = None
-            label_ids = []
-            for word_idx in word_ids:
-                # Special tokens have a word id that is None. We set the label to -100 so they are automatically
-                # ignored in the loss function.
-                if word_idx is None:
-                    label_ids.append(-100)
-                # We set the label for the first token of each word.
-                elif word_idx != previous_word_idx:
-                    label_ids.append(label[word_idx])
-                # For the other tokens in a word, we set the label to either the current label or -100, depending on
-                # the label_all_tokens flag.
-                else:
-                    label_ids.append(label[word_idx] if label_all_tokens else -100)
-                previous_word_idx = word_idx
-            labels.append(label_ids)
-        tokenized_inputs["labels"] = labels
-        return tokenized_inputs
+        try:
+            tokenized_inputs = tokenizer(examples[task_data.sentence1_key], truncation=True, is_split_into_words=True)
+            label_all_tokens = True
+            labels = []
+            for i, label in enumerate(examples[TASK_LABELS[task]]):
+                word_ids = tokenized_inputs.word_ids(batch_index=i)
+                # logger.info(tokenized_inputs)
+                # logger.info(word_ids)
+                # logger.info(label)
+                previous_word_idx = None
+                label_ids = []
+                for word_idx in word_ids:
+                    # Special tokens have a word id that is None. We set the label to -100 so they are automatically
+                    # ignored in the loss function.
+                    if word_idx is None:
+                        label_ids.append(-100)
+                    # We set the label for the first token of each word.
+                    elif word_idx != previous_word_idx:
+                        label_ids.append(label[word_idx])
+                    # For the other tokens in a word, we set the label to either the current label or -100, depending on
+                    # the label_all_tokens flag.
+                    else:
+                        label_ids.append(label[word_idx] if label_all_tokens else -100)
+                    previous_word_idx = word_idx
+                labels.append(label_ids)
+            tokenized_inputs["labels"] = labels
+            return tokenized_inputs
+        except Exception as err:
+            print(err)
 
     if is_text_class_task:
         datasets = task_data.datasets.map(
