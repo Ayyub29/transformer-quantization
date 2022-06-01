8920e739cd4b6524876dc23a8dc11b19a0f69d93
muhammad.ayyub <muhammad.ayyub@intern.bukalapak.com>
2022-06-01 15:32:11 +0700

    test mltilabel

--- main.py
+++ main.py
@@ -220,7 +220,12 @@ def _make_datasets_and_trainer(config, model, model_enum, tokenizer, task, task_
     # tokenize text and define datasets for word classification
     def preprocess_fn_word(examples):
         try:
-            tokenized_inputs = tokenizer(examples[task_data.sentence1_key], truncation=True, is_split_into_words=True)
+            args = (
+                (examples[task_data.sentence1_key],)
+                if task_data.sentence2_key is None
+                else (examples[task_data.sentence1_key], examples[task_data.sentence2_key])
+            )
+            tokenized_inputs = tokenizer(*args, padding=padding, max_length=max_length, truncation=True)
             label_all_tokens = True
             labels = []
             subword_to_word_ids = []
