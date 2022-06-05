2a9f2794c0733621113833f5140fd8e9ad36e0b1
muhammad.ayyub <muhammad.ayyub@intern.bukalapak.com>
2022-06-05 19:18:12 +0700

    test

--- utils/indonlu_task.py
+++ utils/indonlu_task.py
@@ -214,7 +214,7 @@ def make_compute_metric_fn_multilable(task: INDONLU_Task):
         # result = multi_label_metrics(predictions=preds, labels=p.label_ids)
         sigmoid = torch.nn.Sigmoid()
         probs = sigmoid(torch.Tensor(preds))
-        print(preds)
+        print(preds[0])
         # next, use threshold to turn them into integer predictions
         y_pred = np.zeros(probs.shape)
         y_pred[np.where(probs >= 0.5)] = 1
