799387454d90ec9c731a763981cee2c3a011d28f
muhammad.ayyub <muhammad.ayyub@intern.bukalapak.com>
2022-06-05 15:46:34 +0700

    test

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2021 Qualcomm Technologies, Inc.
 # All Rights Reserved.
 
-from .pretrained_bert import BertForMultiLabelClassification, BertForWordClassification
+from .pretrained_bert import BertForSequenceClassification, BertForWordClassification
 from .quantized_bert import QuantizedBertForSequenceClassification, QuantizedBertForWordClassification
 from .quantized_mobilebert import QuantizedMobileBertForSequenceClassification
 from .quantized_roberta import QuantizedRobertaForSequenceClassification
--- models/pretrained_bert.py
+++ models/pretrained_bert.py
@@ -1,52 +1,57 @@
 import torch
+import numpy as np
 from torch import nn
-from torch.nn import CrossEntropyLoss
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 from transformers import  BertPreTrainedModel,  BertModel
+from typing import List, Optional, Tuple, Union
+from transformers.modeling_outputs import SequenceClassifierOutput
 
-class BertForMultiLabelClassification(BertPreTrainedModel):
+class BertForSequenceClassification(BertPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
-        self.num_labels = config.num_labels_list
+        self.num_labels = config.num_labels
+        self.config = config
+
         self.bert = BertModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.classifiers = nn.ModuleList([nn.Linear(config.hidden_size, num_label) for num_label in self.num_labels])
+        classifier_dropout = (
+            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
+        )
+        self.dropout = nn.Dropout(classifier_dropout)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
 
-        self.init_weights()
+        # Initialize weights and apply final processing
+        self.post_init()
 
+    # @add_start_docstrings_to_model_forward(BERT_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
+    # @add_code_sample_docstrings(
+    #     processor_class=_TOKENIZER_FOR_DOC,
+    #     checkpoint=_CHECKPOINT_FOR_SEQUENCE_CLASSIFICATION,
+    #     output_type=SequenceClassifierOutput,
+    #     config_class=_CONFIG_FOR_DOC,
+    #     expected_output=_SEQ_CLASS_EXPECTED_OUTPUT,
+    #     expected_loss=_SEQ_CLASS_EXPECTED_LOSS,
+    # )
+    
     def forward(
         self,
-        input_ids=None,
-        subword_to_word_ids=None,
-        attention_mask=None,
-        token_type_ids=None,
-        position_ids=None,
-        head_mask=None,
-        inputs_embeds=None,
-        labels=None,
-    ):
-        r"""
-        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`, defaults to :obj:`None`):
-            Labels for computing the token classification loss.
-            Indices should be in ``[0, ..., config.num_labels - 1]``.
-
-    Returns:
-        :obj:`tuple(torch.FloatTensor)` comprising various elements depending on the configuration (:class:`~transformers.BertConfig`) and inputs:
-        loss (:obj:`torch.FloatTensor` of shape :obj:`(1,)`, `optional`, returned when ``labels`` is provided) :
-            Classification loss.
-        scores (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length, config.num_labels)`)
-            Classification scores (before SoftMax).
-        hidden_states (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``config.output_hidden_states=True``):
-            Tuple of :obj:`torch.FloatTensor` (one for the output of the embeddings + one for the output of each layer)
-            of shape :obj:`(batch_size, sequence_length, hidden_size)`.
-
-            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
-        attentions (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``config.output_attentions=True``):
-            Tuple of :obj:`torch.FloatTensor` (one for each layer) of shape
-            :obj:`(batch_size, num_heads, sequence_length, sequence_length)`.
-
-            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
-            heads.
-        """
+        input_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.Tensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple[torch.Tensor], SequenceClassifierOutput]:
+        # """
+        # labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+        #     Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
+        #     config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
+        #     `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        # """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         outputs = self.bert(
             input_ids,
@@ -55,25 +60,48 @@ class BertForMultiLabelClassification(BertPreTrainedModel):
             position_ids=position_ids,
             head_mask=head_mask,
             inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
         )
 
-        sequence_output = self.dropout(outputs[1])
-        logits = []
-        for classifier in self.classifiers:
-            logit = classifier(sequence_output)
-            logits.append(logit)
+        pooled_output = outputs[1]
 
-        outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        loss = None
         if labels is not None:
-            loss_fct = CrossEntropyLoss()
-            total_loss = 0
-            for i, (logit, num_label) in enumerate(zip(logits, self.num_labels)):
-                label = labels[:,i]
-                loss = loss_fct(logit.view(-1, num_label), label.view(-1))
-                total_loss += loss
-            outputs = (total_loss,) + outputs
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
 
-        return outputs  # (loss), scores, (hidden_states), (attentions)
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
 
 class BertForWordClassification(BertPreTrainedModel):
     def __init__(self, config):
