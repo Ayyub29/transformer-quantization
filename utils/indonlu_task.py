f31e6711236a958dc8ed75bd8e123fc7c3230579
muhammad.ayyub <muhammad.ayyub@intern.bukalapak.com>
2022-06-13 16:05:12 +0700

    facqa

--- utils/indonlu_task.py
+++ utils/indonlu_task.py
@@ -216,9 +216,10 @@ def make_compute_metric_fn_text(task: INDONLU_Task):
 def make_compute_metric_fn_word(task: INDONLU_Task):
     def fn(p: EvalPrediction):
         predictions, labels = p
+        print(predictions, labels)
         predictions = np.argmax(predictions, axis=2)
         metrics = {}
-        acc, pre, rec, f1, tm_pre, tm_rec, tm_f1 = conll_evaluation(list_hyp, list_label)
+        acc, pre, rec, f1, tm_pre, tm_rec, tm_f1 = conll_evaluation(predictions, labels)
         metrics["ACC"] = acc
         metrics["F1"] = tm_f1
         metrics["REC"] = tm_rec
