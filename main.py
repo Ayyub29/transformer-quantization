be1f4e185a1d8cdfd38b4238d7266770c8a35166
muhammad.ayyub <muhammad.ayyub@intern.bukalapak.com>
2022-06-13 15:30:40 +0700

    facqa

--- main.py
+++ main.py
@@ -227,7 +227,7 @@ def _make_datasets_and_trainer(config, model, model_enum, tokenizer, task, task_
             if task_data.sentence2_key is None
             else (examples[task_data.sentence2_key])
         )
-        tokenized_inputs = tokenizer(*args, truncation=True, max_length=max_length, is_split_into_words=True)
+        tokenized_inputs = tokenizer(*args, truncation=True, is_split_into_words=True)
         label_all_tokens = True
         labels = []
         subword_to_word_ids = []
@@ -258,7 +258,7 @@ def _make_datasets_and_trainer(config, model, model_enum, tokenizer, task, task_
             if task_data.sentence2_key is None
             else (examples[task_data.sentence1_key], examples[task_data.sentence2_key])
         )
-        results = tokenizer(*args, truncation=True, max_length=max_length, is_split_into_words=True)
+        results = tokenizer(*args, truncation=True, is_split_into_words=True)
 
         results["labels"] = labels
         results["subword_to_word_ids"] = subword_to_word_ids
