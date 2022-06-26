5764d49e11001e537468494265340d74dd7a96ba
muhammad.ayyub <muhammad.ayyub@intern.bukalapak.com>
2022-06-26 17:33:24 +0700

    try fix bug

--- models/pretrained_bert.py
+++ models/pretrained_bert.py
@@ -169,7 +169,7 @@ class BertForWordClassification(BertPreTrainedModel):
         for i in range(max_seq_len):
             mask = (subword_to_word_ids == i).unsqueeze(dim=-1)
             print(sequence_output.shape, mask.shape, subword_to_word_ids.shape)
-            print(subword_to_word_ids[i], sequence_output[i])
+            print(subword_to_word_ids[0][i], sequence_output[0][i])
             word_latents.append((sequence_output * mask).sum(dim=1) / mask.sum())
         word_batch = torch.stack(word_latents, dim=1)
 
