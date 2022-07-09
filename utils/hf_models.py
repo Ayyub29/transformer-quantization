3b5e45feb77f98d7e4e21546151889271d47b15b
muhammad.ayyub <muhammad.ayyub@intern.bukalapak.com>
2022-07-09 22:07:56 +0700

    multitext

--- utils/hf_models.py
+++ utils/hf_models.py
@@ -202,6 +202,7 @@ def check_memory_and_inference_time(config, task):
             subwords = tokenizer.encode(sentence)
             subwords = torch.LongTensor(subwords).view(1, -1).to(model.device)
             label = torch.LongTensor(label)
+            print(label.size())
             dataset_memory = checkpoint("Loading the Dataset")
 
             #Forward
