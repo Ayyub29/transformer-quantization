e0126d7430ca9a3b3d566f5f46bb864c415df572
muhammad.ayyub <muhammad.ayyub@intern.bukalapak.com>
2022-07-09 22:57:37 +0700

    multitext

--- utils/hf_models.py
+++ utils/hf_models.py
@@ -206,7 +206,7 @@ def check_memory_and_inference_time(config, task):
             subwords = tokenizer.encode(sentence)
             subwords = torch.LongTensor(subwords).view(1, -1).to(model.device)
             label = torch.LongTensor(label).view(1, -1).to(model.device)
-            print(label.size(), subwords.size())
+            # print(label.size(), subwords.size())
             dataset_memory = checkpoint("Loading the Dataset")
 
             #Forward
@@ -228,9 +228,14 @@ def check_memory_and_inference_time(config, task):
             backward_memory_arr.append((backward_memory[2]/1024.0) - (dataset_memory[2]/1024.0))
             backward_time = time.time() - time_after_forward
             backward_time_arr.append(backward_time)
-            
-            index = torch.topk(logits, k=1, dim=-1)[1].squeeze().item()
-            print(f'Text: {sentence} | Label : {TASK_INDEX2LABEL[task][index]} ({F.softmax(logits, dim=-1).squeeze()[index] * 100:.3f}%)')
+            if (is_text_class_task):
+                index = torch.topk(logits, k=1, dim=-1)[1].squeeze().item()
+                print(f'Text: {sentence} | Label : {TASK_INDEX2LABEL[task][index]} ({F.softmax(logits, dim=-1).squeeze()[index] * 100:.3f}%)')
+            elif is_multilabel_class_task:
+                index = [torch.topk(logit, k=1, dim=-1)[1].squeeze().item() for logit in logits]
+                print(f'Text: {sentence}')
+                for i, label in enumerate(index):
+                    print(f'Label `{TASK_MULTILABELS[task][i]}` : {TASK_INDEX2LABEL[label]} ({F.softmax(logits[i], dim=-1).squeeze()[label] * 100:.3f}%)')
             print(f'Memory Used: Load {load_memory[2]/1024.0 - (start_memory[2]/1024.0)} mb | Forward {forward_memory[2]/1024.0 - (dataset_memory[2]/1024.0)} mb | Backward {backward_memory[2]/1024.0 - (dataset_memory[2]/1024.0)} mb')
             print(f'Time: Forward {forward_time} s | Backward {backward_time} s')
             print()
